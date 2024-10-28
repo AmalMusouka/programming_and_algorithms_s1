3f10983401d3b856a910eee841fac95d871c1d6e
amalmusouka <amalsudhikumar20@gmail.com>
2024-10-28 12:13:06 +0100

    3rd solution

--- Programming1/hot_dogs.py
+++ Programming1/hot_dogs.py
@@ -11,8 +11,7 @@ each_sale.sort()
 
 for i in range(0, len(each_sale)):
     if i == 0:
-        count = [each_sale[i]] * len(each_sale)
-        profit = sum(count)
+        profit = each_sale[i] * len(each_sale)
         if profit > max_profit:
             max_profit = profit
             best_price = each_sale[i]
@@ -20,8 +19,7 @@ for i in range(0, len(each_sale)):
     elif each_sale[i] == each_sale[i - 1]:
         pass
     else:
-        count = [each_sale[i]] * len(each_sale[i:])
-        profit = sum(count)
+        profit = each_sale[i] * len(each_sale[i:])
         if profit > max_profit:
             max_profit = profit
             best_price = each_sale[i]
