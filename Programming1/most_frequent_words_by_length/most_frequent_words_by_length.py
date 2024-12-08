80b7c180407a1862e4a6f7426fb71c36deb72a09
amalmusouka <amalsudhikumar20@gmail.com>
2024-12-08 21:09:49 +0100

    feat: most frequent words by length finalized

--- Programming1/most_frequent_words_by_length/most_frequent_words_by_length.py
+++ Programming1/most_frequent_words_by_length/most_frequent_words_by_length.py
@@ -1,5 +1,6 @@
 import sys
 
+
 # In this exercise, a word is any sequence of consecutive letters that is not preceded or followed by a letter.
 #
 # Letters are any characters in the set {'A' .. 'Z', 'a' .. 'z'}. Case is insignificant: "AARDVARK" and "aardvark" are the same word.
@@ -14,86 +15,105 @@ import sys
 #
 # Notice that if a word occurs only once, you must write "1 occurrence" (not "1 occurrences").
 
-
-
-def merge(a, b, c):
-    i = j = 0
-
-    for k in range(len(c)):
-        if j == len(b):
-            c[k] = a[i]
-            i += 1
-        elif i == len(a):
-            c[k] = b[j]
-            j += 1
-        elif a[i] > b[j]:
-            c[k] = a[i]
-            i += 1
-        else:
-            c[k] = b[j]
-            j += 1
-
-def merge_sort(list):
-    if len(list) < 2 :
-        return
-
-    mid = len(list) // 2
-
-    a = list[:mid]
-    b = list[mid:]
-
-    merge_sort(a)
-    merge_sort(b)
-
-    merge(a, b, list)
-
-# def sort_by_length(list):
-
-
-
-
 def words_from_string(string):
+    words = []
+
     word = ''
-    word_count = dict()
     for letter in string:
 
-        if ord(letter) > ord("z") or ord(letter) < ord("A") :
+        if ord(letter) > ord("z") or ord(letter) < ord("A"):
             if word != '':
-                if word in word_count:
-                    word_count[word] += 1
-                else:
-                    word_count[word] = 1
+                words.append(word)
                 word = ''
                 continue
         else:
             word += letter
-    output = [(v, k) for k, v in word_count.items()]
-    merge_sort(output)
-    return output
-
-# input_string = " "
-# for line in sys.stdin.readlines():
-#     input_string += line
-
-input_string = """Some glory in their birth, some in their skill,
-Some in their wealth, some in their bodies' force,
-Some in their garments, though new-fangled ill,
-Some in their hawks and hounds, some in their horse;
-And every humour hath his adjunct pleasure,
-Wherein it finds a joy above the rest:
-But these particulars are not my measure;
-All these I better in one general best.
-Thy love is better than high birth to me,
-Richer than wealth, prouder than garments' cost,
-Of more delight than hawks or horses be;
-And having thee, of all men's pride I boast:
-Wretched in this alone, that thou mayst take
-All this away and me most wretched make."""
 
 
-test_dict = {'is': [1, 2], 'gfg': [3], 'best': [1, 3, 4]}
+    return words
 
-number_of_words = (words_from_string(input_string))
-# print(sorted(number_of_words, reverse=True, key=lambda x: len(x) > 3))
-# for key, value in number_of_words:
-#     print(value, key)
+
+def solve(input):
+    words = words_from_string(input)
+    tally = {}
+    for word in words:
+        key = len(word)
+
+        if key not in tally:
+            tally[key] = []
+
+        tally[key].append(word)
+
+    return tally
+
+
+def most_frequent_words(arr, n):
+
+    freq = 0
+    res = ""
+
+    for i in range(0, n):
+        count = 1
+        for j in range(i + 1, n):
+            if arr[j] == arr[i]:
+                count += 1
+
+        if count > freq:
+            res = (arr[i]) + " "
+            freq = count
+        elif count == freq:
+            res += (arr[i]) + " "
+            freq = count
+
+    return str(res), freq
+
+
+# input_string = """Some glory in their birth, some in their skill,
+# Some in their wealth, some in their bodies' force,
+# Some in their garments, though new-fangled ill,
+# Some in their hawks and hounds, some in their horse;
+# And every humour hath his adjunct pleasure,
+# Wherein it finds a joy above the rest:
+# But these particulars are not my measure;
+# All these I better in one general best.
+# Thy love is better than high birth to me,
+# Richer than wealth, prouder than garments' cost,
+# Of more delight than hawks or horses be;
+# And having thee, of all men's pride I boast:
+# Wretched in this alone, that thou mayst take
+# All this away and me most wretched make."""
+
+input_string = ""
+for line in sys.stdin.readlines():
+    input_string += line
+
+
+
+
+number_of_words = (solve(input_string.lower()))
+sorted_words = sorted(number_of_words.items())
+
+
+# print(sorted_words)
+
+for key, value in sorted_words:
+    new = most_frequent_words(sorted(value),len(value))
+    if new[1] == 1:
+        print(f"length {key}: {new[0]}({new[1]} occurrence)")
+    else:
+        print(f"length {key}: {new[0]}({new[1]} occurrences)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
