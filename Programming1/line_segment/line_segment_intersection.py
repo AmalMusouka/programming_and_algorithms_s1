3959e0a78a70b8bf322d5667074c8efddd0be00f
amalmusouka <amalsudhikumar20@gmail.com>
2024-12-12 17:48:39 +0100

    chore: line segment bonus initialized

---
+++ Programming1/line_segment/line_segment_intersection.py
@@ -0,0 +1,15 @@
+# The input of the program consists of 8 integers Ax, Ay, Bx, By, Cx, Cy, Dx, Dy that represent the coordinates of 4 points A, B, C, D in the Euclid plane.
+#
+# All numbers are on a single line, separated by spaces.
+#
+# The program should print ANO (Czech for yes), if the line segments AB and CD share at least one point; it should print NE (Czech for no) otherwise.
+#
+# As an example, for the input 0 0 1 1 0 1 1 0, the answer is ANO (yes).
+
+
+
+
+
+
+
+input = "0 0 1 1 0 1 1 0"
