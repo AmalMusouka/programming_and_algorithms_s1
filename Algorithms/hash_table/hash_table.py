993a20379fe31da814b1679d74928bff20eec1ab
amalmusouka <amalsudhikumar20@gmail.com>
2024-12-15 21:55:59 +0100

    feat: final solution for hash_table

--- Algorithms/hash_table/hash_table.py
+++ Algorithms/hash_table/hash_table.py
@@ -47,14 +47,14 @@ class HashMap:
             for i in range(len(old_table)) :
                 if old_table[i] is not None:
                     current_node = old_table[i]
-                    self.set(current_node.key, current_node.value)
+                    self.set(current_node.key, current_node.value, True)
                     count += 1
                     while current_node.next is not None:
                         current_node = current_node.next
-                        self.set(current_node.key, current_node.value)
+                        self.set(current_node.key, current_node.value, True)
                         count += 1
 
-    def set(self, key, value=1):
+    def set(self, key, value=1, rehash=False):
         index = self.my_hash(key)
 
         if self.table[index] is None:
@@ -64,20 +64,26 @@ class HashMap:
             current_node = self.table[index]
             prev_node = None
 
-            while current_node is not None and current_node.key != key:
-                prev_node = current_node
-                current_node = current_node.next
+            if not rehash:
+                while current_node is not None and current_node.key != key:
+                    prev_node = current_node
+                    current_node = current_node.next
 
-            if current_node is not None:
-                current_node.value += value
+                if current_node is not None:
+                    current_node.value = value
+                else:
+                    current_node = Node(key, value, None)
+                    prev_node.next = current_node
+                    self.size += 1
+
+                if self.load_index() > 4:
+                    self.buckets *= 2
+                    self.resize()
             else:
-                current_node = Node(key, value, None)
-                prev_node.next = current_node
+                new_node = Node(key, value, None)
+                new_node.next = current_node
+                self.table[index] = new_node
                 self.size += 1
-
-        if self.load_index() > 4:
-            self.buckets *= 2
-            self.resize()
 
 
     def get(self, key):
@@ -93,10 +99,7 @@ class HashMap:
             else:
                 return None
 
-        first_value = current_node.value
-        current_node.value = None
-
-        return first_value
+        return current_node.value
 
     def remove(self, key):
         index = self.my_hash(key)
@@ -128,6 +131,8 @@ def words_from_string(string):
                 continue
         else:
             word += letter
+    if word != '':
+        words.append(word)
     return words
 
 
@@ -137,19 +142,24 @@ word_occ = []
 text_ended = False
 
 for line in sys.stdin:
-   if line.strip() == '== END ==':
-     text_ended = True
-     continue
-   if text_ended:
-       word_occ.append(line.strip().lower())
-   else:
-       input_string += line.lower()
-
-for word in words_from_string(input_string.lower()):
-    hash_table.set(word.strip())
+    if line.strip() == '== END ==':
+        text_ended = True
+        continue
+    if text_ended:
+        word_occ.append(line.strip().lower())
+    else:
+        words = words_from_string(line.strip().lower())
+        for word in words:
+            if hash_table.get(word) is not None:
+                value = hash_table.get(word)
+                hash_table.set(word, value + 1)
+            else:
+                hash_table.set(word)
 
 print(f"unique words = {hash_table.size}")
 
 for word in word_occ:
     print(word, hash_table.get(word))
+    if hash_table.get(word) is not None:
+        hash_table.remove(word)
 
