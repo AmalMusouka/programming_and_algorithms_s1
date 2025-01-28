205ead7764d1f11f06dd7150d6238caf354e1d57
amalmusouka <amalsudhikumar20@gmail.com>
2025-01-28 14:24:53 +0100

    feat: ver2 - added dev documentation

--- Project/battleships_main.py
+++ Project/battleships_main.py
@@ -1,8 +1,8 @@
 import random
-# size of grid is 10x10
-# number of ships possible to place is 6, each ship takes one spot, ships may be adjacent to form a larger ship.
 
 class Ship:
+    """The main Ship class that stores the (x, y) co-ordinates, length, position and orientation of the Ship as well as all its (x, y) coordinates that aren't the current co-ordinates.
+    Also stores whether the ship is afloat or has sunk, and where all it has been shot."""
     def __init__(self, x, y, length, orientation=None, sunk=False):
         self.x = x
         self.y = y
@@ -34,6 +34,8 @@ class Ship:
             self.sunk = True
 
 class Grid:
+    """The main grid class used to store all the ships that are placed in it, the number of slots it takes, size, and all the co-ordinates that have been hit by a missile while the game is playing.
+    Also converts the co-ordinates into an actual grid which is visible to the user."""
     def __init__(self, count, size = 10):
         self.ships = []
         self.slot_count = count
@@ -42,14 +44,17 @@ class Grid:
         self.matrix = [['.' for _ in range(size)] for _ in range(size)]
 
     def remove_ship(self, ship):
+        """Removes a ship from the class."""
         self.ships.remove(ship)
 
     def add_to_grid(self, ship):
+        """Adds all the ships to the matrix grid."""
         for pos in ship.positions:
             x, y = pos
             self.matrix[x][y] = ship
 
     def add_ship(self, ship):
+        """Adds a ship to the class."""
         x = ship[0]
         y = ship[1]
         length = ship[2]
@@ -60,15 +65,15 @@ class Grid:
 
 
 def get_x(grid_slot):
-    """Returns the x co-ordinate in integer form from user input which is a letter"""
+    """Returns the x co-ordinate in integer form from user input which is a letter."""
     return ord(list(grid_slot)[0].lower()) - ord('a')
 
 def get_y(grid_slot):
-    """Returns the y co-ordinate in integer form from user input which is a digit"""
+    """Returns the y co-ordinate in integer form from user input which is a digit."""
     return int(list(grid_slot)[1])
 
 def check_valid_grid_slot(grid_slot):
-    """To check """
+    """To check if the x and y co-ordinates that the user inputs is valid. That is, the x-coordinate is a character between and including A-J and y-coordinate is a number between and including 0-9."""
     input_grid = list(grid_slot)
     if len(input_grid) != 2:
         return False
@@ -85,6 +90,7 @@ def check_valid_grid_slot(grid_slot):
         return False
 
 def check_if_empty(x, y, length, grid, orientation=None):
+    """To check if the (x, y) co-ordinates does not contain a ship."""
     if orientation == 'h':
         for i in range(length):
             if grid.matrix[x][y + i] != '.':
@@ -104,6 +110,7 @@ def check_if_empty(x, y, length, grid, orientation=None):
     return True
 
 def check_if_in_bounds(x, y, length, orientation=None):
+    """To check if the x and y co-ordinates of the ship are within the bounds of the matrix grid which is 10 x 10."""
     if orientation== 'h':
         if(y + length - 1) >= 10:
             return False
@@ -116,11 +123,13 @@ def check_if_in_bounds(x, y, length, orientation=None):
     return True
 
 def check_if_ship(x, y, grid):
+    """Checks if the chosen (x, y) co-ordinates already has a ship."""
     if isinstance(grid.matrix[x][y], Ship):
         return True
     return False
 
 def check_if_hit(x, y, grid):
+    """Checks if the chosen (x, y) co-ordinates has already been hit by a missile."""
     if isinstance(grid.matrix[x][y], Ship):
         if (x, y) in grid.matrix[x][y].hit_pos:
             return True
@@ -130,6 +139,7 @@ def check_if_hit(x, y, grid):
     return False
 
 def get_coordinates(length):
+    """Takes the user input to get the (x, y) coordinates."""
     print(f"Choose the coordinates and orientation of your {length}-grid box long ship(A0 - J9)")
     correct_grid_slot = False
     while not correct_grid_slot:
@@ -144,6 +154,7 @@ def get_coordinates(length):
                 print("Invalid grid slot, grid slot should be between A0 - J9")
 
 def get_orientation():
+    """Takes the user input to get the orientation of the ship."""
     correct_input = False
     while not correct_input:
         print("Orientation - 'H' for horizontal and 'V' for vertical.")
@@ -155,6 +166,7 @@ def get_orientation():
             return orientation
 
 def get_ship(length, grid):
+    """Used to create a ship from the user inputs if it is valid and in bounds."""
     grid_slot = get_coordinates(length)
     x = get_x(grid_slot)
     y = get_y(grid_slot)
@@ -177,6 +189,7 @@ def get_ship(length, grid):
         grid.add_ship(ship)
 
 def set_player_grid(grid):
+    """Displays the player grid and takes in user input while constantly updating the grid as the ships are formed at their respective coordinates."""
     display_player_grid(grid)
     get_ship(4, grid)
     for _ in range(2):
@@ -187,20 +200,15 @@ def set_player_grid(grid):
         get_ship(2, grid)
 
 def generate_ai_coordinates():
+    """Used to generate (x, y) coordinates of the ship for the AI."""
     return random.randint(0, 9), random.randint(0, 9)
 
 def generate_ai_orientation():
+    """Used to generate the orientation of the ship for the AI."""
     return random.choice(['h', 'v'])
 
-def get_ai_ship(grid, length):
-    x, y = generate_ai_coordinates()
-    if length != 1:
-        ai_orientation = generate_ai_orientation()
-        create_ships_for_ai(x, y, length, grid,  ai_orientation)
-    else:
-        create_ships_for_ai(x, y, length, grid)
-
 def create_ships_for_ai( x, y, length, grid, orientation=None):
+    """Creates the ships for the AI's grid."""
     if check_if_in_bounds(x, y, length, orientation) is False or check_if_empty(x, y, length, grid, orientation) is False:
         get_ai_ship(grid, length)
     else:
@@ -210,7 +218,17 @@ def create_ships_for_ai( x, y, length, grid, orientation=None):
             ship = (x, y, length, orientation)
         grid.add_ship(ship)
 
+def get_ai_ship(grid, length):
+    """Takes the (x, y) coordinates and uses it to call the function that creates the ship, with the orientation or without depending on the length of the ship."""
+    x, y = generate_ai_coordinates()
+    if length != 1:
+        ai_orientation = generate_ai_orientation()
+        create_ships_for_ai(x, y, length, grid,  ai_orientation)
+    else:
+        create_ships_for_ai(x, y, length, grid)
+
 def set_ai_grid(grid):
+    """Creates the grid for the AI."""
     get_ai_ship(grid, 4)
     for _ in range(2):
         get_ai_ship(grid, 3)
@@ -218,6 +236,7 @@ def set_ai_grid(grid):
         get_ai_ship(grid, 2)
 
 def display_player_grid(player_grid):
+    """Displays the players grid with numbering and lettering and spacing between the ships and/or empty spaces."""
     letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     number = '0  1  2  3  4  5  6  7  8  9'
     space = ' '
@@ -237,7 +256,8 @@ def display_player_grid(player_grid):
     print(space, number)
 
 def display_live_grids(player_grid, ai_grid):
-    # to display the grid while hiding the ships
+    """Displays both the players grid and the AI's grid with numbering and lettering as well as spacing between the ships and/or empty spaces and is constantly updated after each round. Also shows how many ship slots have been hit by the user and the AI.
+    Key point: The AI grid is shown without the ships, which are only displayed after they have been hit and the empty slots are shown as misses if hit."""
     letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     number = '0  1  2  3  4  5  6  7  8  9'
     space = ' '
@@ -280,6 +300,8 @@ def display_live_grids(player_grid, ai_grid):
     print(f"Ship slots hit by you = {len(ai_grid.grid_hit)}",  space * 20, f"Ship slots hit by ai = {len(player_grid.grid_hit)}")
 
 def players_move(player_grid, ai_grid):
+    """Takes the user input for firing the missile. If the slot is invalid or the slot is out of bounds, it tries again until a valid input is provided.
+    Also gives feedback on whether the player hit a ship, sank a ship or missed."""
     strike = input("Choose where to fire your missile(A0 - J9):")
     while True:
         if check_valid_grid_slot(strike):
@@ -308,6 +330,7 @@ def players_move(player_grid, ai_grid):
         ai_grid.grid_hit.append((x, y))
 
 def ai_check(x, y, player_grid):
+    """Used to check if the (x, y) coordinates of the AI's missile struck a ship or missed."""
     if player_grid.matrix[x][y] == ".":
         player_grid.matrix[x][y] = "X"
         return False, x, y
@@ -328,12 +351,15 @@ def ai_check(x, y, player_grid):
         return True, x, y
 
 def get_ai_strike_coordinates(ai_shots):
+    """Used to get the (x, y) coordinates for the AI's strike."""
     while True:
         x, y = generate_ai_coordinates()
         if (x, y) not in ai_shots:
             return x, y
 
 def ai_move(player_grid, ai_shots: list, ai_last_ship_hit: list):
+    """Used to dictate the next move of the AI. Takes in a list of all the co-ordinates shot by the AI and the last ship that it hit if there exists one.
+    The AI checks the neighbouring positions if a ship was hit before and if it didn't sink. If the AI hit a ship, it proceeds to give feedback that the AI indeed did so."""
     orientation = None
     hit_multi_grid_ship = False
     x, y = get_ai_strike_coordinates(ai_shots)
@@ -425,6 +451,7 @@ def ai_move(player_grid, ai_shots: list, ai_last_ship_hit: list):
 
 
 def start_game(slots = 18):
+    """The function that starts the game. Displays the grids, takes the user input for both ships and for firing missiles, as well as for the AI. After the game ends, when either the AI or the User gets all the ships, the game ends and provides the results."""
     # game with 8 ships, includes one 4 block long ship, two 3 block long ships and four 2 block long ships = 4 + 6 + 8 = 18 grid_slots
     player_grid = Grid(slots)
     set_player_grid(player_grid)
@@ -440,10 +467,10 @@ def start_game(slots = 18):
         ai_move(player_grid, ai_shots, ai_last_ship_hit)
         display_live_grids(player_grid, ai_grid)
         if len(player_grid.grid_hit) == 18:
-            print("GAME OVER! AI WINS LOSER!")
+            print("GAME OVER! AI WINS!!")
             break
         elif len(ai_grid.grid_hit) == 18:
-            print("GAME OVER! NOBODY LIKES A SHOWOFF.")
+            print("GAME OVER! YOU WIN!!.")
             break
 
 start_game()
